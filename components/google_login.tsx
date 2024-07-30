9bb9e8dcec90e51163b4afba9644292b8cb695c4
shubhambansal2 <shubhambansal1133@gmail.com>
2024-07-30 11:31:09 +0530

    moving back to previous

--- components/google_login.tsx
+++ components/google_login.tsx
@@ -7,36 +7,36 @@ const GoogleLoginComponent = () => {
         const router = useRouter();
         console.log("Google login success. Credential response:", credentialResponse);
         
-        if (credentialResponse && credentialResponse.credential) {
-          console.log("Received credential:", credentialResponse.credential);
-          // Your fetch call would go here
-          fetch('http://127.0.0.1:8000/api/users/google-login/', {
-            method: 'POST',  // Typically, login is done via POST
-            headers: {
-                'Authorization': `Bearer ${credentialResponse.credential}`,
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ token: credentialResponse.credential })  // Include token in body if needed
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
+//         if (credentialResponse && credentialResponse.credential) {
+//           console.log("Received credential:", credentialResponse.credential);
+//           // Your fetch call would go here
+//           fetch('http://127.0.0.1:8000/api/users/google-login/', {
+//             method: 'POST',  // Typically, login is done via POST
+//             headers: {
+//                 'Authorization': `Bearer ${credentialResponse.credential}`,
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify({ token: credentialResponse.credential })  // Include token in body if needed
+//         })
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(data);
 
-            if (data.tokens && data.tokens.access) {
-                // Set the access token in local storage
-                localStorage.setItem('access_token', data.tokens.access);
+//             if (data.tokens && data.tokens.access) {
+//                 // Set the access token in local storage
+//                 localStorage.setItem('access_token', data.tokens.access);
 
-                // Redirect to home page
-                router.push('/');  // Adjust the path as needed
-            }
-        })
-        .catch(error => console.error('Error:', error));
+//                 // Redirect to home page
+//                 router.push('/');  // Adjust the path as needed
+//             }
+//         })
+//         .catch(error => console.error('Error:', error));
 
         
-//     localStorage.setItem('accessToken', tokens.access);
-        } else {
-          console.error("Credential response is missing or invalid");
-        }
+// //     localStorage.setItem('accessToken', tokens.access);
+//         } else {
+//           console.error("Credential response is missing or invalid");
+//         }
       };
 
   return (
